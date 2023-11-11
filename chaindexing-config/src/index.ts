0f9b68ae121b711d17b7f44598e9cc43aab77b4a
Joshua Oladele <jurshsmith@gmail.com>
2023-11-11 13:47:27 +0100

    Update config validation

--- chaindexing-config/src/index.ts
+++ chaindexing-config/src/index.ts
@@ -1,16 +1,35 @@
 import { Chains, Contract } from '@chaindexing';
 import { Repo } from '@chaindexing/repos';
 
-export class Config<Pool, Conn, R extends Repo<Pool, Conn>> {
-  public blocks_per_batch = 20;
-  public handler_interval_ms = 10000;
-  public ingestion_interval_ms = 10000;
+export enum ConfigErrorType {
+  NoContract,
+  NoChain
+}
 
-  constructor(
-    private readonly repo: R,
-    private readonly chains: Chains,
-    private readonly contracts: Contract[]
-  ) {}
+class ConfigError extends Error {
+  constructor(type: ConfigErrorType) {
+    super(ConfigError.getErrorMessage(type));
+  }
+
+  static getErrorMessage(type: ConfigErrorType): string {
+    switch (type) {
+      case ConfigErrorType.NoContract:
+        return 'At least one contract is required';
+      case ConfigErrorType.NoChain:
+        return 'At least one chain is required';
+    }
+  }
+}
+
+export class Config<Pool, Conn, R extends Repo<Pool, Conn>> {
+  public chains: Chains = new Map();
+  public contracts: Contract[] = [];
+
+  public blocks_per_batch = 20;
+  public handler_rate_ms = 10000;
+  public ingestion_rate_ms = 10000;
+
+  constructor(private readonly repo: R) {}
 
   withBlocksPerBatch(blocks_per_batch: number): Config<Pool, Conn, R> {
     this.blocks_per_batch = blocks_per_batch;
@@ -18,15 +37,20 @@ export class Config<Pool, Conn, R extends Repo<Pool, Conn>> {
     return this;
   }
 
-  withHandlerIntervalMs(handler_interval_ms: number): Config<Pool, Conn, R> {
-    this.handler_interval_ms = handler_interval_ms;
+  withHandlerIntervalMs(handler_rate_ms: number): Config<Pool, Conn, R> {
+    this.handler_rate_ms = handler_rate_ms;
 
     return this;
   }
 
-  withIngestionIntervalMs(ingestion_interval_ms: number): Config<Pool, Conn, R> {
-    this.ingestion_interval_ms = ingestion_interval_ms;
+  withIngestionIntervalMs(ingestion_rate_ms: number): Config<Pool, Conn, R> {
+    this.ingestion_rate_ms = ingestion_rate_ms;
 
     return this;
+  }
+
+  validate() {
+    if (this.contracts.length === 0) throw new ConfigError(ConfigErrorType.NoContract);
+    if (this.chains.size === 0) throw new ConfigError(ConfigErrorType.NoChain);
   }
 }
